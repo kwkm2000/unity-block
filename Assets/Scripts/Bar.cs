7da498470625ad9574d3aac211a19f5493b0bdac
kwkm2000 <kwkm.0802@gmail.com>
2020-10-21 15:50:09 +0900

    Barの実装

---
+++ Assets/Scripts/Bar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Bar : MonoBehaviour
+{
+    public Rigidbody2D Body { get; private set; }
+    public RectTransform RectTransform { get; private set; }
+
+    void Awake()
+    {
+        this.Body = gameObject.GetComponent<Rigidbody2D>();
+        this.RectTransform = gameObject.GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        // 左右ボタンが押下されているとバーを移動させる
+        // ただし動かせる範囲からはみ出ないように調整
+        var currentX = this.RectTransform.localPosition.x;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // 右に15動かす
+            // ただし230以上なら230で止める
+            var newX = Mathf.Min(currentX + 15, 230);
+            var pos = new Vector3(newX, this.RectTransform.localPosition.y, 0);
+            this.RectTransform.localPosition = pos;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // 左に15動かす
+            // ただし-230以下なら-230で止める
+            var newX = Mathf.Max(currentX - 15, -230);
+            var pos = new Vector3(newX, this.RectTransform.localPosition.y, 0);
+            this.RectTransform.localPosition = pos;
+        }
+    }
+}
