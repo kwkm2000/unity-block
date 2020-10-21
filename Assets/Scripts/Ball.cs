2866ee95c0312245fcbca8a7a45ea71441f05cb5
kwkm2000 <kwkm.0802@gmail.com>
2020-10-21 15:38:10 +0900

    Ball実装

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void Start()
+    {
+        // このオブジェクトのRigidbody
+        var body = gameObject.GetComponent<Rigidbody2D>();
+
+        // 親のCanvas
+        var canvas = GetComponentInParent<Canvas>();
+
+        // 右上の方向に一定の強さで飛んでいくように
+        var direction = new Vector2(Random.value, 1).normalized;
+        body.velocity = direction * 640 * canvas.transform.localScale.x;
+    }
+}
